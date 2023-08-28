7e2fa2b265bd52c81927470e14be7c82b3a81b35
NomedXD <coovshow1@gmail.com>
2023-08-29 01:12:53 +0300

    HW_25082023 all done 3

--- src/main/java/by/teachmeskills/sneakersshopwebserviceexam/utils/OrderProductDtoConverter.java
+++ src/main/java/by/teachmeskills/sneakersshopwebserviceexam/utils/OrderProductDtoConverter.java
@@ -13,27 +13,23 @@ import java.util.Set;
 public class OrderProductDtoConverter {
     public static List<OrderProductDto> convertInto(List<OrderDto> orderDtoList) {
         List<OrderProductDto> orderProductDtoList = new ArrayList<>();
-        orderDtoList.forEach(orderDto -> {
-            orderDto.getProductList().forEach(productDto -> {
-                orderProductDtoList.add(OrderProductDto.builder()
-                        .productId(productDto.getId())
-                        .productName(productDto.getName())
-                        .productImage(productDto.getImage())
-                        .productDescription(productDto.getDescription())
-                        .categoryId(productDto.getCategoryId())
-                        .productPrice(productDto.getPrice())
-                        .orderId(orderDto.getId())
-                        .totalOrderPrice(orderDto.getPrice())
-                        .orderDate(orderDto.getDate())
-                        .userId(orderDto.getUserId())
-                        .creditCardNumber(orderDto.getCreditCardNumber())
-                        .shippingType(orderDto.getShippingType())
-                        .shippingCost(orderDto.getShippingCost())
-                        .code(orderDto.getCode())
-                        .address(orderDto.getAddress())
-                        .customerNotes(orderDto.getCustomerNotes()).build());
-            });
-        });
+        orderDtoList.forEach(orderDto -> orderDto.getProductList().forEach(productDto -> orderProductDtoList.add(OrderProductDto.builder()
+                .productId(productDto.getId())
+                .productName(productDto.getName())
+                .productImage(productDto.getImage())
+                .productDescription(productDto.getDescription())
+                .categoryId(productDto.getCategoryId())
+                .productPrice(productDto.getPrice())
+                .orderId(orderDto.getId())
+                .totalOrderPrice(orderDto.getPrice())
+                .orderDate(orderDto.getDate())
+                .userId(orderDto.getUserId())
+                .creditCardNumber(orderDto.getCreditCardNumber())
+                .shippingType(orderDto.getShippingType())
+                .shippingCost(orderDto.getShippingCost())
+                .code(orderDto.getCode())
+                .address(orderDto.getAddress())
+                .customerNotes(orderDto.getCustomerNotes()).build())));
         return orderProductDtoList;
     }
 
@@ -66,18 +62,16 @@ public class OrderProductDtoConverter {
     }
 
     private static void addProductsToOrders(List<OrderProductDto> orderProductDtoList, List<OrderDto> orderDtoList) {
-        orderProductDtoList.forEach(orderProductDto -> {
-            orderDtoList.forEach(orderDto -> {
-                if (Objects.equals(orderProductDto.getOrderId(), orderDto.getId())) {
-                    orderDto.getProductList().add(ProductDto.builder()
-                            .id(orderProductDto.getProductId())
-                            .name(orderProductDto.getProductName())
-                            .image(orderProductDto.getProductImage())
-                            .description(orderProductDto.getProductDescription())
-                            .categoryId(orderProductDto.getCategoryId())
-                            .price(orderProductDto.getProductPrice()).build());
-                }
-            });
-        });
+        orderProductDtoList.forEach(orderProductDto -> orderDtoList.forEach(orderDto -> {
+            if (Objects.equals(orderProductDto.getOrderId(), orderDto.getId())) {
+                orderDto.getProductList().add(ProductDto.builder()
+                        .id(orderProductDto.getProductId())
+                        .name(orderProductDto.getProductName())
+                        .image(orderProductDto.getProductImage())
+                        .description(orderProductDto.getProductDescription())
+                        .categoryId(orderProductDto.getCategoryId())
+                        .price(orderProductDto.getProductPrice()).build());
+            }
+        }));
     }
 }
